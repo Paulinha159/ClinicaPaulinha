5b9290898a6a951660a9652ef90f87681b910ef1
Paula Rebesco <paularebesco@gmail.com>
2024-06-25 22:45:09 -0300

    ç

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+/* Criar um sistema que irá gerenciar as consultas de uma clínica médica.
+Como requisitos, nós teremos as seguintes funcionalidades:
+
+Adicionar uma nova consulta
+Listar todas as consultas
+Atualizar uma consulta existente
+Cancelar uma consulta */
+
 const consultas = [];
-let consulta = {};
+let consulta = {}
+let novaConsulta = {};
 let opcao = 0;
 let cadastrandoConsulta = false;
 let removendoConsulta = false;
-let atualizandoConsulta = false;
-let atualizandoIndice = -1;
-let campoAtualizando = '';
 
 console.log("Bem-vindos, escolha uma das opções abaixo:");
 console.log("1: Digitar as informações da consulta:");
@@ -30,16 +36,15 @@ process.stdin.on("data", function (data) {
                 removendoConsulta = true;
                 break;
             case 3:
-                console.log("Digite o nome do paciente a ser atualizado:");
-                atualizandoConsulta = true;
-                break;
+                console.log("Atualize a consulta");
+
             case 4:
                 if (consultas.length === 0) {
                     console.log("Nenhuma consulta cadastrada.");
                 } else {
-                    console.log("Consultas cadastradas:");
+                    console.log("consultas cadastradas:");
                     consultas.forEach((consulta, index) => {
-                        console.log(`${index + 1}. Paciente: ${consulta.paciente}, Médico: ${consulta.medico}, Data: ${consulta.data}, Horário: ${consulta.horario}`);
+                        console.log(`${index + 1}. paciente: ${consulta.paciente}, medico: ${consulta.medico}, data: ${consulta.data}, horario: ${consulta.horario}`);
                     });
                 }
                 opcao = 0;
@@ -49,6 +54,7 @@ process.stdin.on("data", function (data) {
                 console.log("3: Atualizar consulta");
                 console.log("4: Para ver as consultas que inseriu");
                 console.log("5: Sair do programa");
+ 
                 break;
             case 5:
                 console.log("Encerrando o programa...");
@@ -62,6 +68,7 @@ process.stdin.on("data", function (data) {
                 console.log("3: Atualizar consulta");
                 console.log("4: Para ver as consultas que inseriu");
                 console.log("5: Sair do programa");
+                
                 break;
         }
     } else {
@@ -79,7 +86,7 @@ process.stdin.on("data", function (data) {
                         console.log("Horário:");
                     } else if (!consulta.horario) {
                         consulta.horario = entrada_usuario;
-                        consultas.push(consulta);
+                        consultas.push(cadastrandoConsulta);
                         console.log("Consulta cadastrada com sucesso!");
                         consulta = {};
                         opcao = 0;
@@ -90,6 +97,7 @@ process.stdin.on("data", function (data) {
                         console.log("3: Atualizar consulta");
                         console.log("4: Para ver as consultas que inseriu");
                         console.log("5: Sair do programa");
+
                     }
                 }
                 break;
@@ -100,80 +108,20 @@ process.stdin.on("data", function (data) {
                     const index = consultas.findIndex(consulta => consulta.paciente.toLowerCase() === paciente.toLowerCase());
                     if (index !== -1) {
                         consultas.splice(index, 1);
-                        console.log(`Consulta do paciente '${paciente}' removida com sucesso.`);
+                        console.log(`consulta'${paciente}' removida com sucesso.`);
                     } else {
-                        console.log(`Consulta do paciente '${paciente}' não encontrada.`);
+                        console.log(`consulta '${paciente}' não encontrada.`);
                     }
                     opcao = 0;
-                    removendoConsulta = false;
+                    removendoconsulta = false;
+            
                     console.log("\nEscolha uma das opções abaixo:");
                     console.log("1: Digitar as informações da consulta:");
                     console.log("2: Caso queira remover uma consulta");
                     console.log("3: Atualizar consulta");
                     console.log("4: Para ver as consultas que inseriu");
                     console.log("5: Sair do programa");
-                }
-                break;
 
-            case 3:
-                if (atualizandoConsulta) {
-                    if (atualizandoIndice === -1) {
-                        const paciente = entrada_usuario;
-                        atualizandoIndice = consultas.findIndex(consulta => consulta.paciente.toLowerCase() === paciente.toLowerCase());
-                        if (atualizandoIndice !== -1) {
-                            consulta = { ...consultas[atualizandoIndice] };
-                            console.log("Digite o novo nome do paciente (deixe vazio para não alterar):");
-                            campoAtualizando = 'paciente';
-                        } else {
-                            console.log(`Consulta do paciente '${paciente}' não encontrada.`);
-                            opcao = 0;
-                            atualizandoConsulta = false;
-                            atualizandoIndice = -1;
-                            console.log("\nEscolha uma das opções abaixo:");
-                            console.log("1: Digitar as informações da consulta:");
-                            console.log("2: Caso queira remover uma consulta");
-                            console.log("3: Atualizar consulta");
-                            console.log("4: Para ver as consultas que inseriu");
-                            console.log("5: Sair do programa");
-                        }
-                    } else {
-                        if (campoAtualizando === 'paciente') {
-                            if (entrada_usuario) {
-                                consulta.paciente = entrada_usuario;
-                            }
-                            console.log("Digite o novo nome do médico (deixe vazio para não alterar):");
-                            campoAtualizando = 'medico';
-                        } else if (campoAtualizando === 'medico') {
-                            if (entrada_usuario) {
-                                consulta.medico = entrada_usuario;
-                            }
-                            console.log("Digite a nova data (deixe vazio para não alterar):");
-                            campoAtualizando = 'data';
-                        } else if (campoAtualizando === 'data') {
-                            if (entrada_usuario) {
-                                consulta.data = entrada_usuario;
-                            }
-                            console.log("Digite o novo horário (deixe vazio para não alterar):");
-                            campoAtualizando = 'horario';
-                        } else if (campoAtualizando === 'horario') {
-                            if (entrada_usuario) {
-                                consulta.horario = entrada_usuario;
-                            }
-                            consultas[atualizandoIndice] = consulta;
-                            console.log("Consulta atualizada com sucesso!");
-                            consulta = {};
-                            opcao = 0;
-                            atualizandoConsulta = false;
-                            atualizandoIndice = -1;
-                            campoAtualizando = '';
-                            console.log("\nEscolha uma das opções abaixo:");
-                            console.log("1: Digitar as informações da consulta:");
-                            console.log("2: Caso queira remover uma consulta");
-                            console.log("3: Atualizar consulta");
-                            console.log("4: Para ver as consultas que inseriu");
-                            console.log("5: Sair do programa");
-                        }
-                    }
                 }
                 break;
 
@@ -185,7 +133,12 @@ process.stdin.on("data", function (data) {
                 console.log("3: Atualizar consulta");
                 console.log("4: Para ver as consultas que inseriu");
                 console.log("5: Sair do programa");
+
                 break;
         }
     }
-});
+}); 
+
+
+
+
